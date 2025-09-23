8ac0565164d0eb03e6544a9f82637efb87275973
bagusstriutomo <anandabagus121@gmail.com>
2025-09-23 14:29:53 +0700

    update display produk

--- public/index.php
+++ public/index.php
@@ -38,61 +38,61 @@
         <div class="row justify-content-center">
             <div class="col-md-4">
                 <div class="card mb-4">
-                    <img src="UMKM/img/test.jpg" class="card-img-top" alt="gambar produk">
+                    <img src="UMKM/img/laptop.jpg" class="card-img-top" alt="Laptop">
                     <div class="card-body">
                         <h5 class="card-title">DELL XPS 13</h5>
                         <p class="card-text">Laptop premium dengan performa tinggi dan desain elegan.</p>
-                        <a href="#" class="btn btn-primary">Lihat</a>
+                        <a href="#" class="btn btn-primary">Lihat Detail</a>
                     </div>      
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="card mb-4">
-                    <img src="UMKM/img/produk2.jpg" class="card-img-top" alt="gambar produk">
+                    <img src="UMKM/img/smartphone.jpg" class="card-img-top" alt="Smartphone">
                     <div class="card-body">
-                        <h5 class="card-title">Kopi Arabika Gayo</h5>
-                        <p class="card-text">Kopi khas Aceh dengan aroma dan cita rasa yang kuat.</p>
-                        <a href="#" class="btn btn-primary">Lihat</a>
+                        <h5 class="card-title">Samsung Galaxy S24</h5>
+                        <p class="card-text">Smartphone flagship dengan kamera canggih dan layar AMOLED.</p>
+                        <a href="#" class="btn btn-primary">Lihat Detail</a>
                     </div>      
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="card mb-4">
-                    <img src="UMKM/img/produk3.jpg" class="card-img-top" alt="gambar produk">
+                    <img src="UMKM/img/smartwatch.jpg" class="card-img-top" alt="Smartwatch">
                     <div class="card-body">
-                        <h5 class="card-title">Tas Rotan Bali</h5>
-                        <p class="card-text">Tas handmade dari rotan asli Bali, cocok untuk gaya kasual.</p>
-                        <a href="#" class="btn btn-primary">Lihat</a>
+                        <h5 class="card-title">Apple Watch Series 9</h5>
+                        <p class="card-text">Smartwatch modern dengan fitur kesehatan dan notifikasi pintar.</p>
+                        <a href="#" class="btn btn-primary">Lihat Detail</a>
                     </div>      
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="card mb-4">
-                    <img src="UMKM/img/produk4.jpg" class="card-img-top" alt="gambar produk">
+                    <img src="UMKM/img/headphone.jpg" class="card-img-top" alt="Headphone">
                     <div class="card-body">
-                        <h5 class="card-title">Batik Pekalongan</h5>
-                        <p class="card-text">Kain batik motif klasik, cocok untuk acara formal dan santai.</p>
-                        <a href="#" class="btn btn-primary">Lihat</a>
+                        <h5 class="card-title">Sony WH-1000XM5</h5>
+                        <p class="card-text">Headphone wireless dengan noise cancelling terbaik di kelasnya.</p>
+                        <a href="#" class="btn btn-primary">Lihat Detail</a>
                     </div>      
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="card mb-4">
-                    <img src="UMKM/img/produk5.jpg" class="card-img-top" alt="gambar produk">
+                    <img src="UMKM/img/tablet.jpg" class="card-img-top" alt="Tablet">
                     <div class="card-body">
-                        <h5 class="card-title">Keripik Pisang Lampung</h5>
-                        <p class="card-text">Cemilan renyah dan gurih, oleh-oleh khas Lampung.</p>
-                        <a href="#" class="btn btn-primary">Lihat</a>
+                        <h5 class="card-title">iPad Air 2024</h5>
+                        <p class="card-text">Tablet ringan dan powerful untuk produktivitas dan hiburan.</p>
+                        <a href="#" class="btn btn-primary">Lihat Detail</a>
                     </div>      
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="card mb-4">
-                    <img src="UMKM/img/produk6.jpg" class="card-img-top" alt="gambar produk">
+                    <img src="UMKM/img/speaker.jpg" class="card-img-top" alt="Speaker">
                     <div class="card-body">
-                        <h5 class="card-title">Sepatu Kulit Cibaduyut</h5>
-                        <p class="card-text">Sepatu kulit asli buatan pengrajin Cibaduyut, nyaman dan awet.</p>
-                        <a href="#" class="btn btn-primary">Lihat</a>
+                        <h5 class="card-title">JBL Flip 6</h5>
+                        <p class="card-text">Speaker bluetooth portabel dengan suara jernih dan bass kuat.</p>
+                        <a href="#" class="btn btn-primary">Lihat Detail</a>
                     </div>      
                 </div>
             </div>
